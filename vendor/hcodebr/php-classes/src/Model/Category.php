06622856cb8a476de84f48aefafb34dc891a38c1
Silas Gon <silasidc@gmail.com>
2018-05-23 15:57:26 -0300

    Categorias Vs produtos

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -74,5 +74,57 @@ class Category extends Model{
 
     }
 
+    public function getProducts($related = true){
+
+        $sql = new Sql();
+
+        if ($related === true){
+
+           return $sql->select("SELECT * FROM tb_products WHERE idproduct IN(
+                                      SELECT a.idproduct 
+                                      FROM tb_products a 
+                                      INNER JOIN tb_categoriesproducts b ON a.idproduct = b.idproduct
+                                      WHERE b.idcategory = :idcategory
+                                    );
+                                  ", [
+                                      ':idcategory'=>$this->getidcategory()
+            ]);
+
+        }else{
+
+            return $sql->select("SELECT * FROM tb_products WHERE idproduct NOT IN(
+                                      SELECT a.idproduct 
+                                      FROM tb_products a 
+                                      INNER JOIN tb_categoriesproducts b ON a.idproduct = b.idproduct
+                                      WHERE b.idcategory = :idcategory
+                                    );
+                                 ", [
+                                     ':idcategory'=>$this->getidcategory()
+                     ]);
+        }
+    }
+
+    public function addProduct(Product $product){
+
+        $sql = new Sql();
+
+        $sql->query("INSERT INTO tb_categoriesproducts (idcategory, idproduct) VALUES (:idcategory, :idproduct)", [
+                                ':idcategory'=>$this->getidcategory(),
+                                ':idproduct'=>$product->getidproduct()
+        ]);
+
+    }
+
+    public function removeProduct(Product $product){
+
+        $sql = new Sql();
+
+        $sql->query("DELETE FROM tb_categoriesproducts WHERE idcategory = :idcategory AND idproduct = :idproduct", [
+            ':idcategory'=>$this->getidcategory(),
+            ':idproduct'=>$product->getidproduct()
+        ]);
+
+    }
+
 
 }
