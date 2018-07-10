60fc89fea4db2db90578e0bfec872df5a4157431
Silas Gon <silasidc@gmail.com>
2018-07-10 10:30:06 -0300

    Detalhes do produto, categorias

--- views-cache/product-detail.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/product-detail.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,9 +1,9 @@
-<div class="product-big-title-area">
+<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="product-big-title-area">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="product-bit-title text-center">
-                    <h2>Sony{$product.desproduct}</h2>
+                    <h2>Sony<?php echo htmlspecialchars( $product["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></h2>
                 </div>
             </div>
         </div>
@@ -17,23 +17,23 @@
                 <div class="product-content-right">
                     <div class="product-breadcroumb">
                         <a href="/">Home</a>
-                        <a href="">Sony{$product.desproduct}</a>
+                        <a href="">Sony<?php echo htmlspecialchars( $product["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a>
                     </div>
                     
                     <div class="row">
                         <div class="col-sm-6">
                             <div class="product-images">
                                 <div class="product-main-img">
-                                    <img src="{$product.desphoto}">
+                                    <img src="<?php echo htmlspecialchars( $product["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
                                 </div>
                             </div>
                         </div>
                         
                         <div class="col-sm-6">
                             <div class="product-inner">
-                                <h2 class="product-name">Sony{$product.desproduct}</h2>
+                                <h2 class="product-name">Sony<?php echo htmlspecialchars( $product["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></h2>
                                 <div class="product-inner-price">
-                                    <ins>R${function="formatPrice($product.vlprice)"}</ins>
+                                    <ins>R$<?php echo formatPrice($product["vlprice"]); ?></ins>
                                 </div>    
                                 
                                 <form action="" class="cart">
@@ -44,7 +44,7 @@
                                 </form>   
                                 
                                 <div class="product-inner-category">
-                                    <p>Categorias: {loop="$categories"} <a href="/categories/{$value.idcategory}">{$value.descategory}</a>{/loop}.
+                                    <p>Categorias: <?php $counter1=-1;  if( isset($categories) && ( is_array($categories) || $categories instanceof Traversable ) && sizeof($categories) ) foreach( $categories as $key1 => $value1 ){ $counter1++; ?> <a href="/categories/<?php echo htmlspecialchars( $value1["idcategory"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["descategory"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a><?php } ?>.
                                 </div> 
                                 
                                 <div role="tabpanel">
